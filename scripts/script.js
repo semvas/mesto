840d5c6d9d722bd15761535e74c04d2c0717c6c3
IvanSergeyev <ivan.sergeyev29@yandex.ru>
2021-03-10 22:55:34 +0600

    Add render card

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
-let openEditForm = document.querySelector('.profile__edit-btn');
-let closeEditForm = document.querySelector('.popup__close-btn');
-let popup = document.querySelector('.popup');
+const openEditForm = document.querySelector('.profile__edit-btn');
+const closeEditForm = document.querySelector('.popup__close-btn');
+const popup = document.querySelector('.popup');
 
-let profileName = document.querySelector('.profile__name');
-let profileDesc = document.querySelector('.profile__desc');
+const profileName = document.querySelector('.profile__name');
+const profileDesc = document.querySelector('.profile__desc');
 
-let form = document.querySelector('form[name="profile-edit"]');
-let editName = document.querySelector('input[name="profile-name"]');
-let editDesc = document.querySelector('input[name="profile-desc"]');
+const form = document.querySelector('form[name="profile-edit"]');
+const editName = form.querySelector('input[name="profile-name"]');
+const editDesc = form.querySelector('input[name="profile-desc"]');
 
 function openPopup () {
   popup.classList.add('popup_opened');
@@ -26,8 +26,35 @@ function saveProfileEdit (evt) {
   closePopup ();
 }
 
-closeEditForm.addEventListener('click', closePopup);
+const elementsContainer = document.querySelector('.elements');
+const cardTemplate = document.querySelector('.element-template')
+
+function createElement (item) {
+  const cardElement = cardTemplate.content.cloneNode(true);
+
+  const cardTitle = cardElement.querySelector('.element__title');
+  cardTitle.textContent = item.name;
+  
+  const cardImage = cardElement.querySelector('.element__image');
+  cardImage.alt = item.name;
+  cardImage.src = item.link;
+  
+  return cardElement;  
+};
+
+function renderCard () {
+  const card = initialCards.map(item => {
+    const newCard = createElement(item);
+    return newCard;
+  });
+
+  elementsContainer.append(...card);
+}
+
+renderCard();
 
 openEditForm.addEventListener('click', openPopup);
+
+closeEditForm.addEventListener('click', closePopup);
 
 form.addEventListener('submit', saveProfileEdit);
