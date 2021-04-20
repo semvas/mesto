611e52cde10d401e5aaba423c0c44e71c9d7cecd
IvanSergeyev <ivan.sergeyev29@yandex.ru>
2021-04-20 10:37:59 +0600

    move and restruct files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import { Card } from './Card.js';
-import { FormValidator } from './FormValidator.js';
+import { Card } from '../components/Card.js';
+import { FormValidator } from '../components/FormValidator.js';
 
 import {
   selectors,
@@ -25,7 +25,7 @@ import {
   elementsContainer,
   
   initialCards, imagePopup, imageCaption, showImgPopup 
-} from './constants.js';
+} from '../utils/constants.js';
 
 export function openPopup (popup) {
   popup.classList.add('popup_opened');
