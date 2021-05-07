b9353fcb14df742f08877ddd2fa8fa9a42b655bc
IvanSergeyev <ivan.sergeyev29@yandex.ru>
2021-05-07 19:11:43 +0600

    prepare for review

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,22 @@
 export default class Card {
-  constructor(data, cardSelector, handleImgPopup) {
+  constructor(data, cardSelector, userId, { handleCardClick, handleLikeClick, handleDeleteClick }) {
     this._name = data.name;
     this._link = data.link;
+    this._likes = data.likes;
+    this._cardId = data._id;
+    this._ownerId = data.owner._id;
+
     this._cardSelector = cardSelector;
-    this._handleImgPopup = handleImgPopup;
+
+    this._userId = userId;
+    this._isOwner = this._ownerId ===this._userId;
+
+    this._handleCardClick = handleCardClick;
+    this._handleDeleteClick = handleDeleteClick;
+
+    this._handleLikeClick = () => handleLikeClick(data._id,
+      this._likeButton.classList.contains('element__like-btn_act'),
+      arr => this._likeCard(arr));
   }
 
   _getTemplate() {
@@ -14,26 +27,29 @@ export default class Card {
       .cloneNode(true);
   }
 
-  _handleLikeClick() {
-    this._element.querySelector('.element__like-btn').classList.toggle('element__like-btn_act');
-  }
-
-  _handleDeleteCard() {
-    this._element.closest('.element').remove();
-  }
-
   _setEventListeners() {
-    this._cardImage.addEventListener('click', () => {
-      this._handleImgPopup(this._name, this._link);
-    });
+    this._cardImage.addEventListener('click', this._handleCardClick);
 
-    this._likeButton.addEventListener('click', () => {
-      this._handleLikeClick()
-    });
+    this._likeButton.addEventListener('click', this._handleLikeClick);
 
-    this._deleteButton.addEventListener('click', () => {
-      this._handleDeleteCard()
-    });
+    if (this._isOwner) {
+      this._deleteButton.addEventListener('click', this._handleDeleteClick);
+    } else {
+      this._deleteButton.remove();
+    }
+  }
+
+  _likeCard(arr) {
+    if (arr.some(like => like._id === this._userId)) {
+      this._likeButton.classList.add('element__like-btn_act');
+    } else {
+      this._likeButton.classList.remove('element__like-btn_act');
+    }
+    this._element.querySelector('.element__like-counter').textContent = arr.length;
+  }
+
+  deleteCard() {
+    this._element.remove();
   }
 
   createElement() {
@@ -47,6 +63,8 @@ export default class Card {
 
     this._cardImage.src = this._link;
     this._cardImage.alt = this._name;
+
+    this._likeCard(this._likes);
 
     this._setEventListeners();
 
